28a795a705fc45ef6b0519320282e7cdc8e9f26a
John Kevin Basco <basco.johnkevin@gmail.com>
2014-09-25 15:26:36 +0800

    remove unnecessary include of form-errors partial in job detail template

--- app/LaravelPH/Job/Views/job/detail.blade.php
+++ app/LaravelPH/Job/Views/job/detail.blade.php
@@ -4,8 +4,6 @@
 
     <h3>Jobs Detail View</h3>
 
-    @include('partials/form-errors')
-
     <h3>{{ $job->title }}</h3>
     <p>{{ $job->description }}</p>
     <p>{{ $job->author->username }}</p>
